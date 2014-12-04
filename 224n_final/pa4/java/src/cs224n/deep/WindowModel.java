a1718350769c94dc08c789f9970fd447921ca895
Addison Lawrence Weiler <alweiler@corn30.stanford.edu>
2014-12-03 20:20:51 -0800

    Gradients

--- 224n_final/pa4/java/src/cs224n/deep/WindowModel.java
+++ 224n_final/pa4/java/src/cs224n/deep/WindowModel.java
@@ -23,6 +23,7 @@ public class WindowModel {
 	public static final int C_N = 150;
 	public static final int NUM_FEATURES = 5;
 	public static final int N = 50;
+	public static final double alpha = 0.001;
 
 	public void initWeights(){
 		// initialize with bias inside as the last column
@@ -41,7 +42,9 @@ public class WindowModel {
 		// U = SimpleMatrix...
 	}
 
-	public SimpleMatrix feedForward(SimpleMatrix x_1, SimpleMatrix x_2, SimpleMatrix x_3){
+	public void feedForwardAndBackward(SimpleMatrix x_1, SimpleMatrix x_2, SimpleMatrix x_3, SimpleMatrix labelVector){
+		//Forward Propagation
+
 		//Create x vector, 151 x 1
 		SimpleMatrix newX = new SimpleMatrix(C_N + 1, 1);
 		int count = 0;
@@ -50,18 +53,25 @@ public class WindowModel {
 			newX.set(count+N, 0, x_2.get(i, 0));
 			newX.set(count+N * 2, 0, x_3.get(i, 0));
 			count++;
-		}		
+		}
+
 		newX.set(C_N, 0, 1);
 
 		SimpleMatrix m = W.mult(newX);
 		SimpleMatrix newM = new SimpleMatrix(H + 1, 1);
+		
 		for (int i = 0; i < H; i++){
 			newM.set(i, 0, Math.tanh(m.get(i, 0)));
 		}
 		newM.set(H, 0, 1);
-		SimpleMatrix finalMatrix = U.mult(newM);
 
-		return SoftMaxScore(finalMatrix);
+		SimpleMatrix finalMatrix = U.mult(newM);
+		SimpleMatrix sigmoid = SoftMaxScore(finalMatrix);
+
+		//Backpropagation
+		SimpleMatrix newU = updateU(finalMatrix, newM, labelVector);
+		SimpleMatrix newW = updateW(finalMatrix, newM, newX, m, labelVector);
+		SimpleMatrix newL = updateL(finalMatrix, newM, m, labelVector);
 	}
 
 	public SimpleMatrix SoftMaxScore(SimpleMatrix v){
@@ -74,6 +84,56 @@ public class WindowModel {
 			g.set(i, 0, Math.exp(v.get(i,0)) / denominator);
 		}
 		return g;
+	}
+
+	public SimpleMatrix updateU(SimpleMatrix p, SimpleMatrix h, SimpleMatrix labelVector){
+		SimpleMatrix du = (labelVector.minus(p)).mult(h.transpose());
+		return U.minus(du.scale(alpha));
+	}
+
+	public SimpleMatrix updateW(SimpleMatrix p, SimpleMatrix h, SimpleMatrix x, SimpleMatrix m, SimpleMatrix labelVector){
+		SimpleMatrix a = U.transpose().mult(labelVector.minus(p));
+		SimpleMatrix b = new SimpleMatrix(H+1, 1);
+		
+		for (int i = 0; i < a.numRows() - 1; i++){
+			double temp = 1 - Math.pow(Math.tanh(m.get(i, 0)), 2);
+			b.set(i, 0, temp);
+		}
+
+		double temp = 1 - Math.pow(Math.tanh(1), 2);
+		b.set(a.numRows()-1, 0, temp);
+
+		SimpleMatrix finalMatrix = new SimpleMatrix(H+1, 1);
+		//Element-wise multiplication
+		for (int i = 0; i < a.numRows(); i++){
+			finalMatrix.set(a.get(i, 0) * b.get(i, 0));
+		}
+
+		return finalMatrix.mult(x.transpose());
+	}
+
+	public SimpleMatrix updateL(SimpleMatrix p, SimpleMatrix h, SimpleMatrix m, SimpleMatrix labelVector){
+		SimpleMatrix a = U.transpose().mult(labelVector.minus(p));
+		SimpleMatrix b = new SimpleMatrix(H+1, 1);
+		
+		for (int i = 0; i < a.numRows() - 1; i++){
+			double temp = 1 - Math.pow(Math.tanh(m.get(i, 0)), 2);
+			b.set(i, 0, temp);
+		}
+
+		double temp = 1 - Math.pow(Math.tanh(1), 2);
+		b.set(a.numRows()-1, 0, temp);
+
+		SimpleMatrix finalMatrix = new SimpleMatrix(H+1, 1);
+		//Element-wise multiplication
+		for (int i = 0; i < a.numRows(); i++){
+			finalMatrix.set(a.get(i, 0) * b.get(i, 0));
+		}
+
+		SimpleMatrix newW = W.extractMatrix(0, W.numRows(), 0, W.numCols() - 1);
+		finalMatrix = finalMatrix.extractMatrix(0, finalMatrix.numRows() - 1, 0, finalMatrix.numCols());
+
+		return newW.transpose().mult(finalMatrix);
 	}
 
 
@@ -128,14 +188,31 @@ public class WindowModel {
 		}
 	}
 
+	public HashMap<String, Integer> createDict(){
+		HashMap<String, Integer> dict = new HashMap<String, Integer>();
+		String[] s = {"O", "LOC", "MISC", "ORG", "PER"};
+		int count = 0;
+		for (String x : s){
+			dict.put(x, count);
+			count++;
+		}
+		return dict;
+	}
 
 	/**
 	 * Simplest SGD training 
 	 */
 	public void train(List<Datum> _trainData ){
+		HashMap<String, Integer> dict = createDict();
+		
 		SimpleMatrix x = new SimpleMatrix(50, 1);
 		x.set(0, 1);
-		feedForward(x, x, x);
+		String label = "O";
+		SimpleMatrix labelVector = new SimpleMatrix(NUM_FEATURES, 1);
+		labelVector.set(dict.get(label),0, 1);
+
+		feedForwardAndBackward(x, x, x, labelVector);
+
 	}
 
 	
